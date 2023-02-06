94c2123e0be1a258061124df1130dbd6bafa7780
ibra <ibrahimalanshor6@gmail.com>
2023-02-06 16:19:49 +0800

    feat: validate argument

--- src/t9n.js
+++ src/t9n.js
@@ -4,15 +4,22 @@ function T9N(config) {
   this.messages = {};
 
   if (isObject(config)) {
+    if (!isObject(config.messages))
+      throw new Error('config messages must be an object');
+
     this.messages = config.messages;
   }
 }
 
 T9N.prototype.setMessages = function (messages) {
+  if (!isObject(messages)) throw new Error('messages must be an object');
+
   this.messages = messages;
 };
 
 T9N.prototype.setLocaleMessages = function (locale, messages) {
+  if (!isObject(messages)) throw new Error('messages must be an object');
+
   this.messages[locale] = messages;
 };
 
@@ -21,6 +28,9 @@ T9N.prototype.getMessages = function () {
 };
 
 T9N.prototype.getLocaleMessages = function (locale) {
+  if (!this.messages.hasOwnProperty(locale))
+    throw new Error('locale not found');
+
   return this.messages[locale];
 };
 
