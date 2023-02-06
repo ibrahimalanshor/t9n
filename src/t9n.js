49b45d6ccfa169003ce920a3c637ef3139a9f2e2
ibra <ibrahimalanshor6@gmail.com>
2023-02-06 12:11:05 +0800

    feat: getMessages method

---
+++ src/t9n.js
@@ -0,0 +1,15 @@
+const { isObject } = require('../lib/helpers/check-types.helper');
+
+function T9N(config) {
+  this.messages = {};
+
+  if (isObject(config)) {
+    this.messages = config.messages;
+  }
+}
+
+T9N.prototype.getMessages = function () {
+  return this.messages;
+};
+
+module.exports = T9N;
